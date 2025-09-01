9507c36f6ac407183ab03d36e9869cbdb705497e
jakbin <jakbin4747@gmail.com>
2025-09-01 17:51:06 +0530

    add pull function

--- ollama_rich/ollama_rich.py
+++ ollama_rich/ollama_rich.py
@@ -2,6 +2,7 @@ from ollama import Client
 from rich.console import Console
 from rich.markdown import Markdown
 from rich.live import Live
+from ollama_rich.utils import progress
 
 console = Console()
 
@@ -59,3 +60,30 @@ class OllamaRichClient:
             console.print(f"[bold red]Error:[/bold red] {e}")
             return {}
 
+    def pull(self, model, stream: bool = True):
+        try:
+            if stream:
+                console.print(f"[bold green] Pulling model [bold yellow]{model}[/bold yellow] with streaming...[/ bold green]")
+                with progress:
+                    task_id = None
+                    for chunk in self.client.pull(model, stream=True):
+                        # Initialize task when total size is known
+                        if 'total' in chunk and task_id is None:
+                            total_bytes = chunk['total']
+                            task_id = progress.add_task("Downloading", total=total_bytes)
+
+                        # Update progress
+                        if task_id is not None and 'completed' in chunk:
+                            progress.update(task_id, completed=chunk['completed'])
+                    
+                console.print(f"[bold green]:heavy_check_mark: Model '{model}' pulled successfully.[/bold green]")
+
+            else:
+                with console.status("[bold green]Pulling...") as status:
+                    res = self.client.pull(model)
+                    if res["status"] == "success":
+                        console.print(f" [bold green]:heavy_check_mark: Model '{model}' pulled successfully.[/bold green]")
+                    else:
+                        console.print(f"[bold red]Error pulling model '{model}': {res['status']}[/bold red]")
+        except ConnectionError as e:
+            console.print(f"[bold red]Error pulling model '{model}': {e}[/bold red]")
