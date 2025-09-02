4faf083f023b167074760094eb174213f902b54b
jakbin <jakbin4747@gmail.com>
2025-09-02 10:42:22 +0530

    bump version

--- ollama_rich/__init__.py
+++ ollama_rich/__init__.py
@@ -1,6 +1,5 @@
-__version__ = "1.0.2"
+__version__ = "1.0.3"
 
 from .ollama_rich import OllamaRichClient
-from .utils import models_table, model_info_table
 
-__all__ = ["OllamaRichClient", "models_table", "model_info_table"]
+__all__ = ["OllamaRichClient"]
